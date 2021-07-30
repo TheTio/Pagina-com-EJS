55eb1f196fab423d1b62117794e44d7b5f8b3a1a
Gabriel Cardoso <cardoso.gabriel1987@gmail.com>
2021-07-30 15:35:50 -0300

    Update File

--- server.js
+++ server.js
@@ -4,7 +4,40 @@ const app = express()
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('pages/index')
+  const items = [
+    {
+      title: 'D',
+      message: 'esenvolver aplicações/serviços de forma fácil'
+    },
+    {
+      title: 'E',
+      message: 'JS usa JavaScript para renderizar HTML'
+    },
+    {
+      title: 'M',
+      message: 'uito fácil de usar'
+    },
+    {
+      title: 'A',
+      message: 'mando usa-lo'
+    },
+    {
+      title: 'I',
+      message: 'nstall ejs'
+    },
+    {
+      title: 'S',
+      message: 'intaxe simples'
+    }
+  ]
+
+  const subtitle =
+    'Uma linguagem de modelagem para criação de página HTML utilizando JS'
+
+  res.render('pages/index', {
+    qualitys: items,
+    subtitle: subtitle 
+  })
 })
 
 app.get('/sobre', function (req, res) {
